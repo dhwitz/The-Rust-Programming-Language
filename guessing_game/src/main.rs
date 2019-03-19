43ab7bdc4eff1ffb173223606eaedf7784413ba8
David Horowitz <dhorowit@stevens.edu>
2019-03-19 12:19:56 -0400

    Added chapter 9 ex

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,6 +4,24 @@ use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
 
+pub struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess must be between 1 and 100, got {}", value);
+        }
+        Guess {
+            value
+        }
+    }
+    pub fn value(&self) -> u32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number.");
 
